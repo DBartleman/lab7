ccb1974aab062913a85c80621215306875c4aec8
Andrew Bartleman <abartlem@ucsd.edu>
2017-02-23 13:07:28 -0800

    test commit

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -9,5 +9,8 @@ $(document).ready(function() {
  * Function that is called when the document is ready.
  */
 function initializePage() {
-	// your code here
+	$(".project").click(function() {
+		ga('send', 'event', 'like', 'click');
+	}
 }
+
